29c1b5bb69cafad0421fe241c61e9ad1beca8f1c
ethandrew53 <ethandrew@btinternet.com>
2023-11-27 14:34:51 +0000

    Added get and set methods to classes and getDetails to Competitor

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,39 @@
 public class Person {
+    private Name name;
+    private String email;
+    private String country;
+    private int age;
 
+    public Person(Name n, String e, String c, int a)
+    {
+        name = n;
+        email = e;
+        country = c;
+        age = a;
+    }
+
+    public Name getName() {
+        return name;
+    }
+    public void setName(Name name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setDateOfBirth(int age) {
+        this.age = age;
+    }
 }
